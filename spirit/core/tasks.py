64b7d2d15bd66746e33491f7b6c3d07826ea2978
nitely <ecastroborsani@gmail.com>
2020-09-23 04:40:38 -0300

    avatars task refactor

--- spirit/core/tasks.py
+++ spirit/core/tasks.py
@@ -1,21 +1,19 @@
 # -*- coding: utf-8 -*-
 
 import logging
-import io
-import os
 
 from django.db import transaction
 from django.core.mail import send_mail
 from django.apps import apps
 from django.core.management import call_command
 from django.contrib.auth import get_user_model
-from django.core.files.uploadedfile import SimpleUploadedFile
 from django.core.files.storage import default_storage
 
 from PIL import Image
 
 from .conf import settings
 from . import signals
+from .utils.tasks import avatars
 
 logger = logging.getLogger(__name__)
 
@@ -101,40 +99,21 @@ def full_search_index_update():
 
 @delayed_task
 def make_avatars(user_id):
-    def crop_max_square(image):
-        w, h = image.size
-        wh = min(w, h)
-        x = max(0, (w - wh) // 2)
-        y = max(0, (h - wh) // 2)
-        return image.crop((x, y, x+wh, y+wh))
-    def resize_max(image, to):
-        assert min(image.size) == max(image.size), 'not a square'
-        if max(image.size) <= to:
-            return image
-        return image.resize((to, to), resample=Image.BICUBIC)
-    def to_file(image):
-        buff = io.BytesIO()
-        image.save(buff, format='JPEG', subsampling=0, quality=90)
-        return SimpleUploadedFile(
-            'pic.jpg', content=buff.getvalue(), content_type='image/jpeg')
-    def make_small_avatar(name, image):
-        image = resize_max(image, 100)
-        file = to_file(image)
-        name, ext = os.path.splitext(name)
-        default_storage.save('{}_small{}'.format(name, ext), file)
     User = get_user_model()
     user = User.objects.get(pk=user_id)
     user.st.avatar.open()
     image = Image.open(user.st.avatar)
-    square_image = crop_max_square(image)
-    image = resize_max(square_image, 300)
-    file = to_file(image)
+    image = avatars.crop_max_square(image)
+    big_avatar = avatars.thumbnail(image, 300)
     # delete original even for overwrite storage,
     # as it may have other extension
     user.st.avatar.delete()
-    user.st.avatar.save('pic.jpg', file)
+    user.st.avatar.save('pic.jpg', big_avatar)
     user.st.save()
-    make_small_avatar(user.st.avatar.name, square_image)
+    small_avatar = avatars.thumbnail(image, 100)
+    default_storage.save(
+        avatars.with_suffix(user.st.avatar.name, '_small'),
+        small_avatar)
 
 
 @delayed_task
